f6fdec37215b64f0d36721d5f53d7c8cf118864c
Eliseu Lucena Barros <eliseu42lucena@gmail.com>
2019-02-28 19:41:23 -0300

    Main begin file project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+#include "files_begin.h"
+#include "exercice.h"
+
+
+// C++11 Reference
+// https://pt.cppreference.com/w/
+// https://www.onlinegdb.com/online_c++_compiler
+
+int 
+main () {
+   // printf(">>>> Begin\n\n");
+    // main_begin();
+    begin_exercice();
+   // printf(">>>> Finsh\n");
+
+   return 0;
+}
