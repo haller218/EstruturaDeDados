4fc6a51ef0405b80611b885381267d451f2c23dc
Eliseu Lucena Barros <eliseu42lucena@gmail.com>
2019-03-15 17:21:21 -0300

    Monney class of virtual money

---
+++ Monney.h
@@ -0,0 +1,27 @@
+#include <functional>
+#include <string>
+
+/*
+##$ Classe destinada a implementar sistema de trocas por meio de 
+#$     estrutura de dados em lista
+
+
+  -!- Utilizar estrutura de dados
+
+  -1- Criar Classe de operação com arquivos padrão blockchain.
+
+
+
+#$
+##$
+
+*/
+
+
+
+
+class Monney 
+{
+
+  
+};
